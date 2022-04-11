fbdae63b190a323814be0f4e6ce088604e15d87a
Fazil Babu <fazil47babu@gmail.com>
2022-04-11 23:54:52 +0530

    Added texture demos

--- src/components/pages/Textures/Textures.tsx
+++ src/components/pages/Textures/Textures.tsx
@@ -1,11 +1,14 @@
 import Page from "../../Page";
-import { TextureUVDemo } from "./TextureDemos";
+import { CombinedTextureDemo, TextureUVDemo } from "./TextureDemos";
 
 export default function Textures() {
   return (
     <Page pageName="textures">
       <h1>Textures</h1>
+      <h2>Texture Coordinates</h2>
       <TextureUVDemo />
+      <h2>Texture Maps</h2>
+      <CombinedTextureDemo />
     </Page>
   );
 }
