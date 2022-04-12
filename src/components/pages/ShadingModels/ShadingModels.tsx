c927aa9cb5ba69fadd05bb0a95accb8ee5c430dc
Aron <aronc.anand@gmail.com>
2022-04-12 10:22:06 +0530

    added texture description and shading models questions

--- src/components/pages/ShadingModels/ShadingModels.tsx
+++ src/components/pages/ShadingModels/ShadingModels.tsx
@@ -20,14 +20,60 @@ export default function ShadingModels() {
     <Page pageName="shadingModels">
       <h1>Shading Models</h1>
       <h2>Unshaded</h2>
+      <p></p>
       <UnshadedDemo />
       <h2>Flat Shading</h2>
+      <p>
+        Flat shading is the simplest shading model. Each rendered polygon has a
+        single normal vector; shading for the entire polygon is constant across
+        the surface of the polygon. With a small polygon count, this gives
+        curved surfaces a faceted look. We use this shading method if speed is
+        everything.
+      </p>
       <FlatShadingDemo />
       <h2>Gouraud Shading</h2>
+      <p>
+        Gouraud shading is in between the two: like Phong shading, each polygon
+        has one normal vector per vertex, but instead of interpolating the
+        vectors, the color of each vertex is computed and then interpolated
+        across the surface of the polygon.
+      </p>
       <GouraudShadingDemo />
       <h2>Phong Shading</h2>
+      <p>
+        Phong shading is the most sophisticated of the three methods you list.
+        Each rendered polygon has one normal vector per vertex; shading is
+        performed by interpolating the vectors across the surface and computing
+        the color for each point of interest. Interpolating the normal vectors
+        gives a reasonable approximation to a smoothly-curved surface while
+        using a limited number of polygons. We use phong shading if quality is
+        everything.
+      </p>
       <PhongShadingDemo />
       <Quiz quizInfo={quizInfo1} />
+      <Quiz
+        quizInfo={{
+          question: "Which shading model is the simplest of the three?",
+          options: [
+            "Flat shading model",
+            "Phong shading model",
+            "Gouraud shading model",
+          ],
+          answerIndex: 0,
+        }}
+      />
+      <Quiz
+        quizInfo={{
+          question:
+            "Which shading model is used when quality is the primary requirement?",
+          options: [
+            "Flat shading model",
+            "Phong shading model",
+            "Gouraud shading model",
+          ],
+          answerIndex: 1,
+        }}
+      />
     </Page>
   );
 }
--- src/components/pages/Textures/Textures.tsx
+++ src/components/pages/Textures/Textures.tsx
@@ -5,6 +5,13 @@ export default function Textures() {
   return (
     <Page pageName="textures">
       <h1>Textures</h1>
+      <p>
+        In computer graphics, the application of a type of surface to a 3D
+        image. A texture can be uniform, such as a brick wall, or irregular,
+        such as wood grain or marble. The common method is to create a 2D
+        bitmapped image of the texture, called a "texture map," which is then
+        "wrapped around" the 3D object.
+      </p>
       <TextureUVDemo />
     </Page>
   );
