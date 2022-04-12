b68508b95722e74acff668a264a97135733047ca
Aron <aronc.anand@gmail.com>
2022-04-12 09:45:31 +0530

    Description of sweep representation added

--- src/components/pages/Modeling/Modeling.tsx
+++ src/components/pages/Modeling/Modeling.tsx
@@ -10,6 +10,16 @@ export default function Modeling() {
       <h2>Polygonal Mesh Modeling</h2>
       <p>TODO</p>
       <h2>Sweep Representation</h2>
+      <p>
+        Sweep representation is simply turning a 2D object to a 3D object by by
+        sweeping a 2D object over a path. For example, a 2D rectangle moved in a
+        linear path results in the formation of cuboid. To get a better overview
+        of what sweep representation is, lets check the demo below. You can see
+        a small circle but when you sweep it in a circular path using the
+        slider, it leads to the formation of toroid shape. Similarly, we can
+        form cylinder using translational sweep of a circle, prism using a
+        translational sweep of a triangle.
+      </p>
       <SweepRepresentationDemo />
       <h2>Constructive Solid Geometry</h2>
       <CSGDemo />
