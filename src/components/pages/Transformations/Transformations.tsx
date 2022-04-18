a06d9eaec2ccc989b671a7386ffe53149774cf23
Fazil Babu <fazil47babu@gmail.com>
2022-04-18 19:28:15 +0530

    Update Transformations.tsx

--- src/components/pages/Transformations/Transformations.tsx
+++ src/components/pages/Transformations/Transformations.tsx
@@ -10,8 +10,7 @@ import {
 
 export default function Transformations() {
   return (
-    //<Page pageName="transformations">
-    <Page pageName="transformations" pagePaths={{ next: "/shadingModels" }}>
+    <Page pageName="transformations" pagePaths={{ prev: "/projection", next: "/shadingModels" }}>
       <h1>Transformations</h1>
       <p>
         The geometric transformations play a vital role in generating images of
